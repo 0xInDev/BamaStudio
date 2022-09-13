a2648d3f0c397bbdff5e794ea1cfc06130af53ff
Clang Robot <robot@clang-format.com>
2022-09-13 10:27:19 +0000

    🎨 Run clang-format

--- main.cpp
+++ main.cpp
@@ -3,20 +3,21 @@
 #include <QQuickStyle>
 #include <utils.h>
 
-int main(int argc, char *argv[])
-{
-    QGuiApplication app(argc, argv);
-    QQuickStyle::setStyle("Material");
+int main(int argc, char *argv[]) {
+  QGuiApplication app(argc, argv);
+  QQuickStyle::setStyle("Material");
 
-    QQmlApplicationEngine engine;
-    Utils::init();
-    const QUrl url(u"qrc:/BStudio/BStudio/main.qml"_qs);
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-        &app, [url](QObject *obj, const QUrl &objUrl) {
-            if (!obj && url == objUrl)
-                QCoreApplication::exit(-1);
-        }, Qt::QueuedConnection);
-    engine.load(url);
+  QQmlApplicationEngine engine;
+  Utils::init();
+  const QUrl url(u"qrc:/BStudio/BStudio/main.qml"_qs);
+  QObject::connect(
+      &engine, &QQmlApplicationEngine::objectCreated, &app,
+      [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl)
+          QCoreApplication::exit(-1);
+      },
+      Qt::QueuedConnection);
+  engine.load(url);
 
-    return app.exec();
+  return app.exec();
 }
